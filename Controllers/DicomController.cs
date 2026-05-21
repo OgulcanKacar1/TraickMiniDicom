b3ed4a20a1ed0e2e31683c059239032ec4aecfd4
Oğulcan Kacar <ogulcankacarr@gmail.com>
2026-05-21 11:43:12 +0300

    refactor: Remove try-catch blocks from controllers favoring global exception handler

--- Controllers/DicomController.cs
+++ Controllers/DicomController.cs
@@ -29,76 +29,58 @@ public class DicomController: ControllerBase
         //file control
         if (file == null || file.Length == 0)
             return BadRequest("Unvalid file. Please upload a valid DICOM file.");
-            try
-            {
-                //reading file
-                using var stream = file.OpenReadStream();
-                
-                //open file with fo-dicom
-                var dicomFile = await DicomFile.OpenAsync(stream);
-                var dataset = dicomFile.Dataset;
-                
-                //extracting dicom tags
-                string patientName = dataset.GetSingleValueOrDefault(DicomTag.PatientName, "Bilinmeyen Hasta");
-                string studyInstanceUID = dataset.GetSingleValueOrDefault(DicomTag.StudyInstanceUID, "Bilinmeyen Çalışma");
-                string modality = dataset.GetSingleValueOrDefault(DicomTag.Modality,"Bilinmeyen Modality");
-                
-                //converting series to string
-                string series = dataset.GetSingleValueOrDefault(DicomTag.SeriesNumber, 0).ToString();
-                
-                // extracting row pixels and column pixels and merging
-                int rows = dataset.GetSingleValueOrDefault(DicomTag.Rows, 0);
-                int columns = dataset.GetSingleValueOrDefault(DicomTag.Columns, 0);
-                string resolution = $"{rows}x{columns}";
 
-                
-                //creating database model
-                var record = new Study
-                {
-                    PatientName = patientName,
-                    StudyInstanceUID = studyInstanceUID,
-                    Modality = modality,
-                    Series = series,
-                    Resolution = resolution,
-                };
-                
-                //save to database
-                _context.Studies.Add(record);
-                await _context.SaveChangesAsync();
-                
-                return Ok(new {message = "The DICOM file has been successfully uploaded and saved to the database.", data = record});
-                
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Dosya İşlenirken bir hata oluştu: {ex.Message}");
-            }
+        //reading file
+        using var stream = file.OpenReadStream();
         
+        //open file with fo-dicom
+        var dicomFile = await DicomFile.OpenAsync(stream);
+        var dataset = dicomFile.Dataset;
+        
+        //extracting dicom tags
+        string patientName = dataset.GetSingleValueOrDefault(DicomTag.PatientName, "Bilinmeyen Hasta");
+        string studyInstanceUID = dataset.GetSingleValueOrDefault(DicomTag.StudyInstanceUID, "Bilinmeyen Çalışma");
+        string modality = dataset.GetSingleValueOrDefault(DicomTag.Modality,"Bilinmeyen Modality");
+        
+        //converting series to string
+        string series = dataset.GetSingleValueOrDefault(DicomTag.SeriesNumber, 0).ToString();
+        
+        // extracting row pixels and column pixels and merging
+        int rows = dataset.GetSingleValueOrDefault(DicomTag.Rows, 0);
+        int columns = dataset.GetSingleValueOrDefault(DicomTag.Columns, 0);
+        string resolution = $"{rows}x{columns}";
+
+        
+        //creating database model
+        var record = new Study
+        {
+            PatientName = patientName,
+            StudyInstanceUID = studyInstanceUID,
+            Modality = modality,
+            Series = series,
+            Resolution = resolution,
+        };
+        
+        //save to database
+        _context.Studies.Add(record);
+        await _context.SaveChangesAsync();
+        
+        return Ok(new {message = "The DICOM file has been successfully uploaded and saved to the database.", data = record});
     }
 
     [HttpGet]
     [Authorize]
     public async Task<IActionResult> GetAllStudies([FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10)
     {
-        try
-        {
-            if (pageIndex < 1) pageIndex = 1;
-            if(pageSize < 1) pageSize = 10;
+        if (pageIndex < 1) pageIndex = 1;
+        if(pageSize < 1) pageSize = 10;
 
-            var studies = await _context.Studies
-                .OrderByDescending(s => s.Id)
-                .Skip((pageIndex - 1) * pageSize)
-                .Take(pageSize)
-                .ToListAsync();
-        
-            return Ok(studies);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, $"Veriler getirilirken bir hata oluştu: {e.Message}");
-        }
+        var studies = await _context.Studies
+            .OrderByDescending(s => s.Id)
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    
+        return Ok(studies);
     }
-    
-    
-    
 }
